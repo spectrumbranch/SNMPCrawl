3972477d7c4f9f01964eb0c8cda5c6ba2e393068
spectrumbranch <spectrum@spectrumbranch.com>
2011-12-02 14:16:35 -0500

    First commit.
    Signed-off-by: Chris
    <spectrum@spectrumbranch.com>

---
+++ SNMPCrawl/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace SNMPCrawl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.communityLabel = new System.Windows.Forms.Label();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setSNMPWalkFilepathToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadServerListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.communityTextBox = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.numServersLoadedLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.snmpwalkLoadedLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.versGroupBox = new System.Windows.Forms.GroupBox();
+            this.radioVers3 = new System.Windows.Forms.RadioButton();
+            this.radioVers2c = new System.Windows.Forms.RadioButton();
+            this.radioVers1 = new System.Windows.Forms.RadioButton();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.versGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // communityLabel
+            // 
+            this.communityLabel.AutoSize = true;
+            this.communityLabel.Location = new System.Drawing.Point(9, 50);
+            this.communityLabel.Name = "communityLabel";
+            this.communityLabel.Size = new System.Drawing.Size(61, 13);
+            this.communityLabel.TabIndex = 0;
+            this.communityLabel.Text = "Community:";
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(76, 76);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(231, 42);
+            this.submitBtn.TabIndex = 1;
+            this.submitBtn.Text = "Start Crawl";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(388, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setSNMPWalkFilepathToolStripMenuItem,
+            this.loadServerListToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // setSNMPWalkFilepathToolStripMenuItem
+            // 
+            this.setSNMPWalkFilepathToolStripMenuItem.Name = "setSNMPWalkFilepathToolStripMenuItem";
+            this.setSNMPWalkFilepathToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.setSNMPWalkFilepathToolStripMenuItem.Text = "&Set SNMPWalk Filepath";
+            this.setSNMPWalkFilepathToolStripMenuItem.Click += new System.EventHandler(this.setSNMPWalkFilepathToolStripMenuItem_Click);
+            // 
+            // loadServerListToolStripMenuItem
+            // 
+            this.loadServerListToolStripMenuItem.Name = "loadServerListToolStripMenuItem";
+            this.loadServerListToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.loadServerListToolStripMenuItem.Text = "&Load Server List";
+            this.loadServerListToolStripMenuItem.Click += new System.EventHandler(this.loadServerListToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // communityTextBox
+            // 
+            this.communityTextBox.Location = new System.Drawing.Point(76, 50);
+            this.communityTextBox.Name = "communityTextBox";
+            this.communityTextBox.Size = new System.Drawing.Size(231, 20);
+            this.communityTextBox.TabIndex = 3;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.numServersLoadedLabel,
+            this.toolStripStatusLabel1,
+            this.snmpwalkLoadedLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 132);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(388, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // numServersLoadedLabel
+            // 
+            this.numServersLoadedLabel.AutoSize = false;
+            this.numServersLoadedLabel.Name = "numServersLoadedLabel";
+            this.numServersLoadedLabel.Size = new System.Drawing.Size(130, 17);
+            this.numServersLoadedLabel.Text = "No Server File Loaded";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(10, 17);
+            this.toolStripStatusLabel1.Text = "|";
+            // 
+            // snmpwalkLoadedLabel
+            // 
+            this.snmpwalkLoadedLabel.AutoSize = false;
+            this.snmpwalkLoadedLabel.Name = "snmpwalkLoadedLabel";
+            this.snmpwalkLoadedLabel.Size = new System.Drawing.Size(130, 17);
+            this.snmpwalkLoadedLabel.Text = "SNMPwalk Not Loaded";
+            // 
+            // versGroupBox
+            // 
+            this.versGroupBox.Controls.Add(this.radioVers3);
+            this.versGroupBox.Controls.Add(this.radioVers2c);
+            this.versGroupBox.Controls.Add(this.radioVers1);
+            this.versGroupBox.Location = new System.Drawing.Point(313, 28);
+            this.versGroupBox.Name = "versGroupBox";
+            this.versGroupBox.Size = new System.Drawing.Size(69, 90);
+            this.versGroupBox.TabIndex = 5;
+            this.versGroupBox.TabStop = false;
+            this.versGroupBox.Text = "Version";
+            // 
+            // radioVers3
+            // 
+            this.radioVers3.AutoSize = true;
+            this.radioVers3.Location = new System.Drawing.Point(14, 65);
+            this.radioVers3.Name = "radioVers3";
+            this.radioVers3.Size = new System.Drawing.Size(31, 17);
+            this.radioVers3.TabIndex = 2;
+            this.radioVers3.TabStop = true;
+            this.radioVers3.Text = "3";
+            this.radioVers3.UseVisualStyleBackColor = true;
+            // 
+            // radioVers2c
+            // 
+            this.radioVers2c.AutoSize = true;
+            this.radioVers2c.Checked = true;
+            this.radioVers2c.Location = new System.Drawing.Point(14, 42);
+            this.radioVers2c.Name = "radioVers2c";
+            this.radioVers2c.Size = new System.Drawing.Size(37, 17);
+            this.radioVers2c.TabIndex = 1;
+            this.radioVers2c.TabStop = true;
+            this.radioVers2c.Text = "2c";
+            this.radioVers2c.UseVisualStyleBackColor = true;
+            // 
+            // radioVers1
+            // 
+            this.radioVers1.AutoSize = true;
+            this.radioVers1.Location = new System.Drawing.Point(14, 19);
+            this.radioVers1.Name = "radioVers1";
+            this.radioVers1.Size = new System.Drawing.Size(31, 17);
+            this.radioVers1.TabIndex = 0;
+            this.radioVers1.TabStop = true;
+            this.radioVers1.Text = "1";
+            this.radioVers1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 154);
+            this.Controls.Add(this.versGroupBox);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.communityTextBox);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.communityLabel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "SNMPCrawl by Chris Bebry [v0.2.1]";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.versGroupBox.ResumeLayout(false);
+            this.versGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label communityLabel;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadServerListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox communityTextBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel numServersLoadedLabel;
+        private System.Windows.Forms.ToolStripMenuItem setSNMPWalkFilepathToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel snmpwalkLoadedLabel;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.GroupBox versGroupBox;
+        private System.Windows.Forms.RadioButton radioVers3;
+        private System.Windows.Forms.RadioButton radioVers2c;
+        private System.Windows.Forms.RadioButton radioVers1;
+    }
+}
+
---
+++ SNMPCrawl/Form1.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+// TO ADD: show name of input file for servers in the status bar.
+namespace SNMPCrawl
+{
+    /* SNMPCrawl
+     * Copyright 2011 Chris Bebry
+     * 
+     * Release Notes:
+     *      0.2.1 :
+     *          -Added radio buttons to choose the version of SNMP user wishes to use.
+     *          -Did a little cleanup, changed an instance of checking for blank/null to use the string utility.
+     *          -Fixed a bug that would output a blank file if we were repeatedly using the same single server file.
+     *          -Increased pixel size of numServersLoadedLabel.
+     *      0.2.0 : 
+     *          -Resolved issue with memory leaks in the COM object causing the earlier speed-up procedure to fail.
+     *          -Cleared outputList after each save, so multiple uses in a single program instance can occur serially.
+     *      0.1.1 : 
+     *          -Fixed "processing current server" index value on status bar. It was incorrectly one less than what it should've been.
+     *          -Greatly sped up processing of successful snmpwalks so that the process closes after confirming the walk is a success.
+     *          -Fixed some output grammar to know when the number of servers is plural or singular.
+     *          -Removed overseen duplicate status field update when loading a server file.
+     *          -Added status label which states whether or not the SNMPwalk file has been loaded.
+     * */
+    public partial class Form1 : Form
+    {
+        private const string OUTPUT_FOR_UNKNOWNHOST = "Server {0} is an unknown host.";
+        private const string OUTPUT_FOR_TIMEOUT = "Server {0} is timing out.";
+        private const string OUTPUT_FOR_SUCCESSFUL = "Server {0} is successful.";
+
+        private const string VERSION_1 = "1";
+        private const string VERSION_2c = "2c";
+        private const string VERSION_3 = "3";
+
+        private string _currVersion = "";
+
+        string snmpwalkFilepath { get; set; }
+        string serverListFilepath { get; set; }
+        private string serverName;
+        private List<string> serverList;
+        private List<string> outputFile;
+
+        private Process _p;
+
+        private string _prevServ = "";
+        private string _lastResult = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            serverList = new List<string>();
+            outputFile = new List<string>();
+            snmpwalkFilepath = "";
+            serverName = "";
+        }
+
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+            object temp = ValidateForm() ? XRunCrawl() : ValidateErrorMessage();
+        }
+
+        private object ValidateErrorMessage()
+        {
+            MessageBox.Show("Please input a Community, locate the file for snmpwalk, or load a server list.");
+            return default(object);
+        }
+
+        /* OLD VERSION
+        private object RunCrawl()
+        {
+            //do stuff
+            string community = communityTextBox.Text;
+            foreach (string server in serverList)
+            {
+                serverName = server;
+                Process p = new Process();
+                p.StartInfo.FileName = snmpwalkFilepath;
+
+                p.StartInfo.Arguments = "-v 2c -c " + community + " " + server;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.StartInfo.UseShellExecute = false;
+                p.EnableRaisingEvents = true;
+                p.StartInfo.CreateNoWindow = true;
+
+                //output handlers
+                p.OutputDataReceived += p_DataReceived;
+                p.ErrorDataReceived += p_DataReceived;
+
+                p.Start();
+                p.BeginErrorReadLine();
+                p.BeginOutputReadLine();
+                numServersLoadedLabel.Text = "Processing Server " + (outputFile.Count+1) + @"/" + serverList.Count;
+                p.WaitForExit();
+            }
+
+            numServersLoadedLabel.Text = "Processed all " + serverList.Count + " servers";
+
+            SaveOutputFile();
+
+            MessageBox.Show("done");
+
+            return default(object);
+        }
+        */
+        private object XRunCrawl()
+        {
+            string community = communityTextBox.Text;
+            _currVersion = GetVersion();
+            _prevServ = "";
+
+            foreach (string server in serverList)
+            {
+                serverName = server;
+                _p = null;
+                _p = new Process();
+                _p.StartInfo.FileName = snmpwalkFilepath;
+
+                _p.StartInfo.Arguments = "-v " + _currVersion + " -c " + community + " " + server;
+                _p.StartInfo.RedirectStandardOutput = true;
+                _p.StartInfo.RedirectStandardError = true;
+                _p.StartInfo.UseShellExecute = false;
+                _p.EnableRaisingEvents = true;
+                _p.StartInfo.CreateNoWindow = true;
+
+                //output handlers
+                _p.OutputDataReceived += p_DataReceived;
+                _p.ErrorDataReceived += p_DataReceived;
+
+                _p.Start();
+                _p.BeginErrorReadLine();
+                _p.BeginOutputReadLine();
+                numServersLoadedLabel.Text = "Processing Server " + (outputFile.Count + 1) + @"/" + serverList.Count;
+                _p.WaitForExit();
+            }
+
+            if (serverList.Count == 1)
+            {
+                numServersLoadedLabel.Text = "Processed the " + serverList.Count + " server";
+            }
+            else
+            {
+                numServersLoadedLabel.Text = "Processed all " + serverList.Count + " servers";
+            }
+            
+
+            SaveOutputFile();
+            outputFile.Clear();
+            //MessageBox.Show("done");
+
+            return default(object);
+        }
+
+        #region Version
+        private string GetVersion()
+        {
+            if (radioVers1.Checked)
+            {
+                return VERSION_1;
+            } 
+            else if (radioVers2c.Checked)
+            {
+                return VERSION_2c;
+            }
+            else if (radioVers3.Checked)
+            {
+                return VERSION_3;
+            }
+            //if for some reason all fails, default to:
+            return VERSION_2c;
+        }
+        #endregion Version
+
+        #region Validation
+        private bool ValidateForm()
+        {
+            if (communityTextBox.Text != "" &&
+                snmpwalkFilepath != "" &&
+                serverList.Count != 0) //good
+            {
+                return true;
+            }
+            else //bad
+            {
+                return false;
+            }
+        }
+        #endregion Validation
+
+        public void p_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            //output will be in e.Data;
+            string output = e.Data;
+            if (_prevServ != serverName) //to avoid redundancy
+            {
+                if (!String.IsNullOrEmpty(output)) //ignore blank outputs
+                {
+                    //MessageBox.Show(output);
+                    //if (output.Substring(0, 7) == "Timeout")
+                    if (output.IndexOf("Timeout") != -1) //if Timeout exists. <-- this version was a change for version 3 snmpwalk calls
+                    {
+                        outputFile.Add(String.Format(OUTPUT_FOR_TIMEOUT, serverName));
+                        // MessageBox.Show(String.Format(OUTPUT_FOR_TIMEOUT, serverName));
+                        _lastResult = OUTPUT_FOR_TIMEOUT;
+                    }
+                    else if (output.Substring(0, 22) == "snmpwalk: Unknown host")
+                    {
+                        outputFile.Add(String.Format(OUTPUT_FOR_UNKNOWNHOST, serverName));
+                        // MessageBox.Show(String.Format(OUTPUT_FOR_UNKNOWNHOST, serverName));
+                        _lastResult = OUTPUT_FOR_UNKNOWNHOST;
+                    }
+                    else
+                    {
+                        outputFile.Add(String.Format(OUTPUT_FOR_SUCCESSFUL, serverName));
+                        //MessageBox.Show(String.Format(OUTPUT_FOR_SUCCESSFUL, serverName));
+                        _lastResult = OUTPUT_FOR_SUCCESSFUL;
+                    }
+                }
+
+                _prevServ = serverName;
+            }
+            else
+            {
+                if (_p != null)
+                {
+                    try
+                    {
+                        if (_lastResult == OUTPUT_FOR_SUCCESSFUL)
+                        {
+                            _p.Kill();
+                        }
+                        else
+                        {
+                            _p.Close();
+                        }
+                    }
+                    catch //im not proud of this, but it works.
+                    {
+                        //hidden catch haha.
+                        //MessageBox.Show(x.ToString());
+                    }
+                }
+            }
+
+        }
+        #region Exit
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion Exit
+
+        #region LoadServerList
+        private void LoadServerList(string filepath)
+        {
+            serverList.Clear();
+            using (StreamReader sr = new StreamReader(filepath))
+            {
+                while(!sr.EndOfStream)
+                {
+                    serverList.Add(sr.ReadLine());
+                }
+                if (serverList.Count == 1)
+                {
+                    numServersLoadedLabel.Text = serverList.Count + " server loaded.";
+                }
+                else
+                {
+                    numServersLoadedLabel.Text = serverList.Count + " servers loaded.";
+                }
+            }
+        }
+
+        private void loadServerListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "c:\\";
+            ofd.Filter = "All files (*.*)|*.*";
+            ofd.FilterIndex = 2;
+            ofd.RestoreDirectory = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    LoadServerList(ofd.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error. Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
+        }
+        #endregion LoadServerList
+
+        private void SaveOutputFile()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text File|*.txt";
+            sfd.Title = "Save the output text file";
+            if (sfd.ShowDialog() != DialogResult.Cancel)
+            {
+                if (sfd.FileName != "")
+                {
+                    System.IO.File.WriteAllLines(sfd.FileName, outputFile);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Cancel was clicked. In order to save the file, you must run the report again and specify a valid filename.");
+            }
+        }
+
+        private void setSNMPWalkFilepathToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "c:\\";
+            ofd.Filter = "SNMPWALK.EXE|snmpwalk.exe";
+            ofd.FilterIndex = 2;
+            ofd.RestoreDirectory = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    snmpwalkFilepath = ofd.FileName;
+                    snmpwalkLoadedLabel.Text = "SNMPwalk Loaded";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error. Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+
+        #region JUNK
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        #endregion JUNK
+    }
+}
